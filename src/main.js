92e4330098f6cb3fcae5910514ce18336c8cd6a5
bingyaoxing <bingyaoxing@gmail.com>
2025-04-07 16:07:26 +0800

    0
    0

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Price Elasticity</h1>
+    <div class="card">
+      <p>Welcome to Price Elasticity Analysis Tool</p>
+    </div>
+  </div>
+`
